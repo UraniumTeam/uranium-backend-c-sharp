d11ba6f309e5f1487f6604e648faaeec44160220
Daniil Lapygin <38853974+MaiNoTee@users.noreply.github.com>
2022-07-06 13:22:21 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using СSharp_Profiler;
+
+using var profiler = new ProfileFunc();
+
+namespace СSharp_Profiler
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Test();
+        }
+
+        [ProfileFunc]
+        private static void Test()
+        {
+            var count = 0;
+            for (var i = 0; i < 2; i++)
+            {
+                count++;
+            }
+        }
+    }
+}
